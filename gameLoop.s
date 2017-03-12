22489980847970bc7265a45014addf68854e4220
Nicolas Lama-Solet <nicolas@lama-solet.com>
2017-03-12 16:38:45 -0700

    Drew a cactus

--- gameLoop.s
+++ gameLoop.s
@@ -2,8 +2,9 @@
 gameLoop:
 
 	call drawTrex
+  call drawCactus
 
-	ld bc, 32766			; space key 
+	ld bc, 32766			; space key
 	in a, (c)			; see if space key is pressed
 	rra				; outermost bit = key space
 	push af				; remember the value
@@ -19,63 +20,82 @@ gameLoop:
 drawTrex:
 
 	ld hl, trex1                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne first cell (left uper coner)
+  ld (charCellAddress), hl      ; draw tne first cell (left uper coner)
 	ld hl, $000F
 	ld (charCellCoord), hl		; (row = 0F, col = 00)
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 
 	ld hl, trex2                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw the second cell (right uper coner)
         ld hl, $010F		      ; at the bottom right of the screen
         ld (charCellCoord), hl	      ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 
 	ld hl, trex3                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne third cell (left bottom coner)
         ld hl, $0010                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 
 	ld hl, trex4                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne forth cell (right bottom coner)
         ld hl, $0110                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-	
+	call copyCharCellAndAttrByteToUpdateList
+
 	ret
 
+drawCactus:
+
+  ld hl, cactus1                 ; hl = key cell's address
+  ld (charCellAddress), hl      ; draw tne first cell (upper part)
+  ld hl, (cactusRowCol)           ; draw
+  ld (charCellCoord), hl    ;
+  call copyCharCellAndAttrByteToUpdateList
+
+  ld hl, cactus2                ; hl = key cell's address
+  ld (charCellAddress), hl      ; draw tne first cell (lower part)
+  ld hl, (cactusRowCol)
+  ld a, l
+  add $1                      ; one row down from rest of cactus
+  ld l, a
+  ld (charCellCoord), hl    ; (row = 0F, col = 10)
+  call copyCharCellAndAttrByteToUpdateList
+
+  ret
+
 trexJump:
-	
+
 	; 1. Remove old trex
 	ld hl, black                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne first cell (left uper coner)
 	ld hl, $000F
 	ld (charCellCoord), hl		; (row = 0F, col = 00)
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 	ld hl, black                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw the second cell (right uper coner)
         ld hl, $010F		      ; at the bottom right of the screen
         ld (charCellCoord), hl	      ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 	ld hl, black                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne third cell (left bottom coner)
         ld hl, $0010                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 	ld hl, black                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne forth cell (right bottom coner)
         ld hl, $0110                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
-	
-	; 2. Halt for a bit	
+
+	; 2. Halt for a bit
 	halt
 	halt
 	halt
@@ -86,28 +106,27 @@ trexJump:
         ld (charCellAddress), hl      ; draw tne first cell (left uper coner)
         ld hl, $000C			; at the bottom right of the screen
         ld (charCellCoord), hl		; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 
 	ld hl, trex2                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw the second cell (right uper coner)
         ld hl, $010C		      ; at the bottom right of the screen
         ld (charCellCoord), hl	      ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
+	call copyCharCellAndAttrByteToUpdateList
 
 	ld hl, trex3                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne third cell (left bottom coner)
         ld hl, $000D                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 
 	ld hl, trex4                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne forth cell (right bottom coner)
         ld hl, $010D                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 	; 4. Halt for a bit
 	halt
@@ -116,41 +135,48 @@ trexJump:
 	halt
 	halt
 	halt
-	
+
 
 	; 5. Remove new trex
 	ld hl, black                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne first cell (left uper coner)
         ld hl, $000C			; at the bottom right of the screen
         ld (charCellCoord), hl		; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 
 	ld hl, black                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw the second cell (right uper coner)
         ld hl, $010C		      ; at the bottom right of the screen
         ld (charCellCoord), hl	      ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 
 	ld hl, black                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne third cell (left bottom coner)
         ld hl, $000D                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 
 	ld hl, black                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne forth cell (right bottom coner)
         ld hl, $010D                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+	call copyCharCellAndAttrByteToUpdateList
 
 	ret
 
-trex1: defb $00, $00, $00, $00, $00, $81, $83, $C7   
-trex2: defb $7E, $DF, $FF, $FF, $F0, $FC, $E0, $E0 
+trex1: defb $00, $00, $00, $00, $00, $81, $83, $C7
+trex2: defb $7E, $DF, $FF, $FF, $F0, $FC, $E0, $E0
 trex3: defb $FF, $7F, $3F, $1F, $0D, $0C, $08, $0C
 trex4: defb $F8, $E8, $E0, $C0, $80, $80, $80, $C0
+
+cactus1: defb $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+cactus2: defb $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+cactusFramesPerMove: defb $05
+cactusFPMCounter: defb $00
+cactusDirection: defb $01  ; 1 is left
+cactusRowCol: defb $0F, $0F
 
 black: defb $00, $00, $00, $00, $00, $00, $00, $00
