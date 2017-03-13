d317f2dc74af7ab233b50a58c7cd5b99dfb35979
itstehkman <itstehkman@gmail.com>
2017-03-13 14:57:33 -0700

    WOHOOO got a jumping trex using a velocity metric

--- gameLoop.s
+++ gameLoop.s
@@ -1,14 +1,31 @@
+        ;; 1. Set trex1=$000F, trex2=$010F, trex3=$0010, trex4=$0110
+        ld hl, $000F                ; h = col, l = row
+        ld (trex1), hl
+        
+        ld hl, $010F
+        ld (trex2), hl
+
+        ld hl, $0010
+        ld (trex3), hl
+
+        ld hl, $0110
+        ld (trex4), hl
+
+	call drawTrex
 
 gameLoop:
 
-	call drawTrex
+        ;; 1. Listen for keys
 
 	ld bc, 32766			; space key 
 	in a, (c)			; see if space key is pressed
 	rra				; outermost bit = key space
 	push af				; remember the value
-	call nc, trexJump		; it's being pressed, then jump t-rex
+	call nc, jumpTrex		; it's being pressed, then jump t-rex
 	pop af
+
+        ;; 2. Loop through all your sprites, and update them (hard coded logic for trex, cactus, etc...)
+        call updateTrex
 
 	ei
         halt                            ; wait for interrupt to print our shit
@@ -16,135 +33,182 @@ gameLoop:
         jp gameLoop
 
 
+;; ---- FUNCTION updateTrex -----
+;;
+;; draws the trex accordingly depending on if he's jumping, what direction he's in in the jump, etc
+;;
+updateTrex:
+        ;; 1. Check if he's jumping
+        ld a, (trex_is_jumping)
+        cp 1                            ; check if 1 (jumping). z = 0 if jumping
+        jp nz, updateTrex_nojump        ; if not jumping, dont do anything
+
+        ;; 2. if jumping, update the trex frame counter
+        ld a, (trex_f)                   
+        ld b, a                         ; b = frequency to update trex if jumping
+        ld a, (trex_f_ctr)              ; a = frame counter
+        cp b                            ; check if f_ctr == f 
+        jp nz, updateTrex_noupdate
+        ld a, 0                         ; if f_ctr == f, then reset f to 0
+        ld (trex_f_ctr), a              ; set trex_f_ctr = 0
+        jr updateTrex_update            ; yes, we need to move the trex this frame
+updateTrex_noupdate:
+        inc a                           ; incremenet trex_f_ctr
+        ld (trex_f_ctr), a              ; set trex_f_ctr = a
+        jp updateTrex_nojump            ; dont need to do anything else, done
+
+        ;; 2a. Since frame counter == update frequency, then we need to move trex
+updateTrex_update:
+        ;; 2b. check which direction we should update him
+        ld a, (trex_up_or_down)         
+        cp 1                            ; check if trex_up_or_down is 1 (up). if so, z = 0
+        jr nz, updateTrex_movedown      ; if z != 0, then must move down
+        
+        ;; 2b-1. move up
+
+        ;; draw black on trex3 and trex4
+        ld hl, black
+        ld (charCellAddress), hl        
+        ld hl, (trex3)                  ; h = col, l = row
+        ld (charCellCoord), hl)
+	call copyCharCellAndAttrByteToUpdateList	
+
+        ld hl, black
+        ld (charCellAddress), hl        
+        ld hl, (trex4)                  ; h = col, l = row    
+        ld (charCellCoord), hl)
+	call copyCharCellAndAttrByteToUpdateList	
+        
+        ;; move all the sprite coords one row less (up the screen)
+        ld hl, (trex1)                  
+        dec l
+        ld (trex1), hl
+        ld hl, (trex2)                  
+        dec l
+        ld (trex2), hl
+        ld hl, (trex3)                  
+        dec l
+        ld (trex3), hl
+        ld hl, (trex4)                  
+        dec l
+        ld (trex4), hl                      ; note: l = row for trex4
+
+        ;; check if its at the top of the trajectory
+        
+        ld a, (trex_jump_height)            
+        ld b, a                             ; b = trex_jump_height
+        ld a, $10                           ; a = trex4 row start (1 row above the ground)
+        
+        sub b                               ; a = a - b  (the max height allowed)
+        
+        cp l                                ; check if trex4 row == max row allowed
+        jp nz, updateTrex_endchangedir
+        ld a, $00
+        ld (trex_up_or_down), a             ; set dir to down (0)
+updateTrex_endchangedir:
+        ;; draw!!
+        call drawTrex
+
+        jr updateTrex_checkifdone       ; go see if we need to set is_jumping to false
+updateTrex_movedown:
+        ;; 2b-2. move down
+
+        ;; draw black on trex1 and trex2
+        ld hl, black
+        ld (charCellAddress), hl        
+        ld hl, (trex1)                  ; h = row, l = row
+        ld (charCellCoord), hl)
+	call copyCharCellAndAttrByteToUpdateList	
+
+        ld hl, black
+        ld (charCellAddress), hl        
+        ld hl, (trex2)                  ; h = col, l = row    
+        ld (charCellCoord), hl)
+	call copyCharCellAndAttrByteToUpdateList	
+        
+        ;; move all the sprite coords one row more (down the screen)
+        ld hl, (trex1)                  
+        inc l
+        ld (trex1), hl
+        ld hl, (trex2)                  
+        inc l
+        ld (trex2), hl
+        ld hl, (trex3)                  
+        inc l
+        ld (trex3), hl
+        ld hl, (trex4)                  
+        inc l
+        ld (trex4), hl                      ; note: l = row for trex4
+
+        ;; draw!!
+        call drawTrex
+        
+updateTrex_checkifdone:
+        ;; 2d. if his row coordinate is the same as the floor, set is_jumping to false
+       
+        ld hl, (trex4)                      ; h = col, l = row
+        ld a, $10
+        cp l                                ; check if trex4 row == ground row
+        jr nz, updateTrex_nojump
+        ld a, $00       
+        ld (trex_is_jumping), a             ; reset is_jumping to 0 (false)
+        ld (trex_f_ctr), a                  ; reset f_ctr to 0
+updateTrex_nojump:
+        ;; 3. Done
+        ret
+
+
+
+;; ---- FUNCTION updateTrex -----
+;;
+;; draws the trex accordingly depending on if he's jumping, what direction he's in in the jump, etc
+;;
 drawTrex:
 
 	ld hl, trex1+3                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne first cell (left uper coner)
-	ld hl, $000F
+	ld hl, (trex1)                  ; h = row, l = col
 	ld (charCellCoord), hl		; (row = 0F, col = 00)
 	call copyCharCellAndAttrByteToUpdateList	
 
 
 	ld hl, trex2+3                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw the second cell (right uper coner)
-        ld hl, $010F		      ; at the bottom right of the screen
+        ld hl, (trex2)		      ; at the bottom right of the screen
         ld (charCellCoord), hl	      ; load the coordinate to draw cell
 	call copyCharCellAndAttrByteToUpdateList	
 
 
 	ld hl, trex3+3                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne third cell (left bottom coner)
-        ld hl, $0010                  ; at the bottom right of the screen
+        ld hl, (trex3)                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
 	call copyCharCellAndAttrByteToUpdateList	
 
 
 	ld hl, trex4+3                  ; hl = key cell's address
         ld (charCellAddress), hl      ; draw tne forth cell (right bottom coner)
-        ld hl, $0110                  ; at the bottom right of the screen
+        ld hl, (trex4)                  ; at the bottom right of the screen
         ld (charCellCoord), hl        ; load the coordinate to draw cell
 	call copyCharCellAndAttrByteToUpdateList	
 	
 	ret
 
-trexJump:
-	
-	; 1. Remove old trex
-	ld hl, black                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne first cell (left uper coner)
-	ld hl, $000F
-	ld (charCellCoord), hl		; (row = 0F, col = 00)
-	call copyCharCellAndAttrByteToUpdateList	
+;; ---- FUNCTION updateTrex -----
+;;
+;; draws the trex accordingly depending on if he's jumping, what direction he's in in the jump, etc
+;;
 
-	ld hl, black                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw the second cell (right uper coner)
-        ld hl, $010F		      ; at the bottom right of the screen
-        ld (charCellCoord), hl	      ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+jumpTrex:
+        ;; 1. if already jumping, ignore request
+        ld a, (trex_is_jumping)         
+        cp 1                        ; if jumping, then z flag is 0
+        jr z, jumpTrex_end
 
-	ld hl, black                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne third cell (left bottom coner)
-        ld hl, $0010                  ; at the bottom right of the screen
-        ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
+        ;; 2. set set state to jumping. updateTrex will take care of actually jumping him
+        ld a, $01
+        ld (trex_is_jumping), a     ; is_jumping = 1     
+        ld (trex_up_or_down), a     ; up_or_down = up (1)
 
-	ld hl, black                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne forth cell (right bottom coner)
-        ld hl, $0110                  ; at the bottom right of the screen
-        ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
-	
-	; 2. Halt for a bit	
-	halt
-	halt
-	halt
-
-
-	; 3. Draw new trex
-	ld hl, trex1+3                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne first cell (left uper coner)
-        ld hl, $000C			; at the bottom right of the screen
-        ld (charCellCoord), hl		; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
-
-	ld hl, trex2+3                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw the second cell (right uper coner)
-        ld hl, $010C		      ; at the bottom right of the screen
-        ld (charCellCoord), hl	      ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
-
-	ld hl, trex3+3                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne third cell (left bottom coner)
-        ld hl, $000D                  ; at the bottom right of the screen
-        ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
-
-	ld hl, trex4+3                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne forth cell (right bottom coner)
-        ld hl, $010D                  ; at the bottom right of the screen
-        ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
-	; 4. Halt for a bit
-	halt
-	halt
-	halt
-	halt
-	halt
-	halt
-	
-
-	; 5. Remove new trex
-	ld hl, black                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne first cell (left uper coner)
-        ld hl, $000C			; at the bottom right of the screen
-        ld (charCellCoord), hl		; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
-
-	ld hl, black                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw the second cell (right uper coner)
-        ld hl, $010C		      ; at the bottom right of the screen
-        ld (charCellCoord), hl	      ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
-
-	ld hl, black                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne third cell (left bottom coner)
-        ld hl, $000D                  ; at the bottom right of the screen
-        ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
-
-	ld hl, black                  ; hl = key cell's address
-        ld (charCellAddress), hl      ; draw tne forth cell (right bottom coner)
-        ld hl, $010D                  ; at the bottom right of the screen
-        ld (charCellCoord), hl        ; load the coordinate to draw cell
-	call copyCharCellAndAttrByteToUpdateList	
-
-	ret
-
+jumpTrex_end:
+        ret
--- sprites.s
+++ sprites.s
@@ -1,28 +1,33 @@
 ;; FORMAT OF SPRITES
-;; <char cell coord> <attr byte> <8 bytes char cell> <dir> <velocity> <velocity counter>
+;; <char cell coord> <attr byte> <8 bytes char cell>
 ;;
 ;; char cell coord is in the format (col, row)  = 2 bytes
 ;;
 ;; attr byte is a single attribute byte         = 1 byte
 ;;
 ;; char cell                                    = 8 bytes
-;;
-;; dir is which direction to move 
-;; (0000 = still, 0001 = left, 0010 = right     = 1 byte
-;; (0100 = up, 1000 = down)                 
-;;
-;; velocity is how many frames to wait to move  = 1 byte
-;; the char cell in that direction
-;;
-;; velocity counter is the current frame ctr    = 1 byte
 ;; 
 ;; ---------
-;; TOTAL                                        = 14 bytes
+;; TOTAL                                        = 11 bytes
 
-;;---------------col row  attr  -----------sprite---------------------  dir vel  vel ctr
-trex1:      defb $00, $00, $06, $00, $00, $00, $00, $00, $81, $83, $C7, $00, $00, $00   
-trex2:      defb $00, $00, $06, $7E, $DF, $FF, $FF, $F0, $FC, $E0, $E0, $00, $00, $00   
-trex3:      defb $00, $00, $06, $FF, $7F, $3F, $1F, $0D, $0C, $08, $0C, $00, $00, $00   
-trex4:      defb $00, $00, $06, $F8, $E8, $E0, $C0, $80, $80, $80, $C0, $00, $00, $00   
+;;---------------col row  attr  -----------sprite--------------------- 
+trex1:      defb $00, $00, $06, $00, $00, $00, $00, $00, $81, $83, $C7
+trex2:      defb $00, $00, $06, $7E, $DF, $FF, $FF, $F0, $FC, $E0, $E0
+trex3:      defb $00, $00, $06, $FF, $7F, $3F, $1F, $0D, $0C, $08, $0C
+trex4:      defb $00, $00, $06, $F8, $E8, $E0, $C0, $80, $80, $80, $C0
 
+;; trex fields -
+;; is_jumping   = if he's jumping (1) or not (0)
+;; up_or_down   = if jumping, then lets us know if he's going up (1) or down (0)
+;; jump_height  = how many cells to jump up
+;; f            = the frame frequency to move the trex up or down
+;; f_ctr        = the current frame counter. when it hits f, move trex up or down
+trex_is_jumping:    defb $00
+trex_up_or_down:    defb $00
+trex_jump_height:   defb $04
+trex_f:             defb $02
+trex_f_ctr:         defb $00
+
+
+;; general all 0 background sprite. does not have col row and attr
 black:      defb $00, $00, $00, $00, $00, $00, $00, $00
